a793506726b12de51f6167d40d9c97e8250d4d21
Daniel Sasser <danielbsasser@gmail.com>
2020-02-14 04:14:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       binding.pry
-      if supplies.include? == "BBQ"
+      if supplies.include?("BBQ")
         found_holidays << holiday
       end
     end 
